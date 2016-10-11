767e045eb7f3a6b546f28aaa14677395b8a86888
Gabriella Simon <gb@gabriellasimon.me>
2016-10-11 13:07:30 +0200

    Get names into collapsible form

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-
-
 $(document).ready(function() {
 
-
+// getting the data from the reviews.json file.
    $.ajax({
       url:'reviews.json',
       dataType: 'json',
-      cache: false,
-      contentType: 'application/json',
     })
     .done(function(data) {
-      console.log(data);
+      console.log(data[0].fullName);
+        $('.collapsible').html('');
+      data.forEach(function(i) {
+        $('.collapsible').append('<li><div class="collapsible-header"><i class="material-icons">filter_drama</i>' + i.fullName + '</div></li>');
+      });
+
     })
     .fail(function(xhr, status, error) {
       console.log(status);
-
     });
 });
